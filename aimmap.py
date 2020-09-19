d493945229b221d6656f68ac9082491a38a783f7
pokkagrinti <60540228+pokkagrinti@users.noreply.github.com>
2020-09-19 21:04:35 +0800

    Cleaned up code and PEP 8

--- aimmap.py
+++ aimmap.py
@@ -1,145 +1,173 @@
 import pygame
-import time
 import random
 import keyboard
 import os
 import win32gui
 import win32api
-import win32con
 
 os.environ['SDL_VIDEO_CENTERED'] = "1"
- 
-pygame.init()
- 
-display_width = 1344
-display_height = 756
- 
-black = (0,0,0)
-white = (255,255,255)
 
-green = (0, 200, 0)
-bright_green = (0, 255, 0)
+# Global Variables
+DISPLAY_WIDTH = 1344
+DISPLAY_HEIGHT = 756
 
-red = (200,0,0)
-bright_red = (255,0,0)
- 
-gameDisplay = pygame.display.set_mode((display_width,display_height))
-pygame.display.set_caption('Aim map')
-clock = pygame.time.Clock()
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
-def text_objects(text, font):
-    
-    textSurface = font.render(text, True, black)
+GREEN = (0, 200, 0)
+BRIGHT_GREEN = (0, 255, 0)
+
+RED = (200, 0, 0)
+BRIGHT_RED = (255, 0, 0)
+
+RED_BUTTON_SIZE = 20
+
+
+def text_objects(text, fontsize):
+    """Set text's size and color"""
+    font = pygame.font.SysFont(None, fontsize)
+    textSurface = font.render(text, True, BLACK)
     return textSurface, textSurface.get_rect()
 
-def click(x,y):
-    x1, y1 = 0, 0
+
+def button(screen, x, y):
+    mouse = pygame.mouse.get_pos()
+    if x + 20 > mouse[0] > x and y + 20 > mouse[1] > y:
+        pygame.draw.rect(screen, BRIGHT_RED, (x, y, 20, 20))
+        return mouse
+    else:
+        pygame.draw.rect(screen, RED, (x, y, 20, 20))
+    return False
+
+
+def draw_red_button(screen, x, y):
+    "Draw an interactive red button on screen"
+    mouse_position = pygame.mouse.get_pos()
+    mouse_pos_x = mouse_position[0]
+    mouse_pos_y = mouse_position[1]
+
+    if x + RED_BUTTON_SIZE > mouse_pos_x > x and y + RED_BUTTON_SIZE > mouse_pos_y > y:
+        pygame.draw.rect(screen, BRIGHT_RED, (x, y, RED_BUTTON_SIZE, RED_BUTTON_SIZE))
+        return mouse_position
+    else:
+        pygame.draw.rect(screen, RED, (x, y, RED_BUTTON_SIZE, RED_BUTTON_SIZE))
+    return False
+
+
+def draw_green_button(screen, x, y):
+    "Draw an interactive green button on screen"
+    mouse_position = pygame.mouse.get_pos()
+    mouse_pos_x = mouse_position[0]
+    mouse_pos_y = mouse_position[1]
+
+    if x + 150 > mouse_pos_x > x and y + 75 > mouse_pos_y > y:
+        pygame.draw.rect(screen, BRIGHT_GREEN, (x, y, 150, 75))
+        return True
+    else:
+        pygame.draw.rect(screen, GREEN, (x, y, 150, 75))
+    return False
+
+
+def timer_text(screen, time_left):
+    """Display a countdown Timer in the game"""
+    textSurf, textRect = text_objects("Timer: " + str(time_left), 25)
+
+    # Set Timer text at top right corner
+    return screen.blit(textSurf, (1257, 10))
+
+
+def points_text(screen, points):
+    """Display the current point in the game"""
+    textSurf, textRect = text_objects("Points: " + str(points), 25)
+
+    # Set Points text at top left corner
+    return screen.blit(textSurf, (10, 10))
+
+
+def click(x, y):
+    """Aimbot easter egg in the game."""
     fg_win = win32gui.GetForegroundWindow()
     x1, y1 = win32gui.ClientToScreen(fg_win, (x, y))
     x1 += 1
     y1 += 1
-    win32api.SetCursorPos((x1,y1))
-    
-
-def normal_button(x, y):
-    mouse = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
-    if x+150 > mouse[0] > x and y+75 > mouse[1] > y:
-        pygame.draw.rect(gameDisplay, bright_green,(x, y, 150,75))
-        return True
-    else:
-        pygame.draw.rect(gameDisplay, green,(x, y,150,75))
-    return False
-
-def button(x, y):
-    mouse = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
-    if x+20 > mouse[0] > x and y+20 > mouse[1] > y:
-        pygame.draw.rect(gameDisplay, bright_red,(x, y,20,20))
-        return True
-    else:
-        pygame.draw.rect(gameDisplay, red,(x, y,20,20))
-    return False
-    
-
-def game_intro():
-        
-        intro = True
-        x = random.randint(96, 1248)
-        y = random.randint(54, 702)
-        store = False
-        points = 0
-        counter = 30
-        green_button = False
-
-        timesup  = True
-        font = pygame.font.SysFont(None, 25)
-        timer = font.render("Timer: "+str(counter), True, black)
-        pygame.time.set_timer(pygame.USEREVENT, 1000)
-        while intro:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                if event.type == pygame.MOUSEBUTTONUP:
-                    if green_button:
-                        counter = 30
-                        points = 0
-                        green_button = False
-                        timesup = False
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if store:
-                        x = random.randint(96, 1248)
-                        y = random.randint(100, 700)
-                        points += 1
-                    
-                        
-                if event.type == pygame.USEREVENT: 
-                    counter -= 1
-                
-                    timer = font.render("Timer: "+str(counter), True, black)
-                    if counter <= 0:
-                        timer = font.render("" , True, black)
-                        store = False
-                        timesup = True
-            gameDisplay.fill(white)
-
-            if not timesup:   
-                text = font.render("Points: "+str(points), True, black)
-                
-                gameDisplay.blit(timer,(1257,10))
-                gameDisplay.blit(text,(10,10))
-                store = button(x, y)
-                if keyboard.is_pressed('q'):
-                    timesup  = True
-                if keyboard.is_pressed('w'):
-                    click(x ,y)
-                
-            else:
-                fontGameOver = pygame.font.SysFont(None, 100)
-                textSurf, textRect = text_objects("Game Over!", fontGameOver)
-                textRect.center = (display_width / 2), (display_height / 2 -200)
-                gameDisplay.blit(textSurf, textRect)
-                
-                fontPoints = pygame.font.SysFont(None, 70)
-                
-                textSurf, textRect = text_objects("Points: "+str(points), fontPoints)
-                textRect.center = (display_width / 2), (display_height / 2 -100)
-                gameDisplay.blit(textSurf, textRect)
-                
-                green_button = normal_button(597, 350)
-                    
-                    
-                
-            pygame.display.update()
-            clock.tick(60)
-
-    
- 
-game_intro()
-
-print("Game Ended!")
-    
-pygame.quit()
-quit()
+    win32api.SetCursorPos((x1, y1))
 
 
+def main():
+    clock = pygame.time.Clock()
+    pygame.init()
+    screen = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+    pygame.display.set_caption('Aim map')
+
+    button_location_x = random.randint(86, 1238)
+    button_location_y = random.randint(44, 692)
+
+    # Game Information
+    points = 0
+
+    timer_secs = 30
+    pygame.time.set_timer(pygame.USEREVENT, 1000)
+
+    cursor_coordinates = False
+
+    times_up = True
+    reset = True
+
+    done = 0
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = 1
+                break
+            elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                if cursor_coordinates:
+                    points += 1
+                    button_location_x = random.randint(86, 1238)
+                    button_location_y = random.randint(44, 692)
+                elif reset:
+                    timer_secs = 30
+                    points = 0
+                    reset = False
+                    times_up = False
+            elif event.type == pygame.USEREVENT:
+                if timer_secs > 0:
+                    timer_secs -= 1
+                else:
+                    times_up = True
+
+                    # Disable red button
+                    cursor_coordinates = False
+
+        screen.fill(WHITE)
+
+        if times_up:
+            textSurf, textRect = text_objects("Game Over!", 100)
+            textRect.center = (DISPLAY_WIDTH / 2), (DISPLAY_HEIGHT / 2 - 200)
+            screen.blit(textSurf, textRect)
+
+            textSurf, textRect = text_objects("Points: " + str(points), 70)
+            textRect.center = (DISPLAY_WIDTH / 2), (DISPLAY_HEIGHT / 2 - 100)
+            screen.blit(textSurf, textRect)
+
+            reset = draw_green_button(screen, 597, 350)
+
+
+        else:
+            if keyboard.is_pressed('q'):
+                times_up = True
+            if keyboard.is_pressed('w'):
+                # Easter egg
+                click(button_location_x, button_location_y)
+
+            timer_text(screen, timer_secs)
+            points_text(screen, points)
+
+            # If cursor coordinates is in the button, store it in cursor_coordinates
+            cursor_coordinates = draw_red_button(screen, button_location_x, button_location_y)
+
+        pygame.display.update()
+        clock.tick(60)
+
+
+if __name__ == "__main__":
+    main()
